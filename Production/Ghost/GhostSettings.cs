8dc469ff465708977391f842abd6bcfe6408e81d
Patrrck Kelly <hey@butterflydesin.me>
2020-12-25 23:21:44 -0700

    change API

--- Production/Ghost/GhostSettings.cs
+++ Production/Ghost/GhostSettings.cs
@@ -5,6 +5,6 @@ public sealed class GhostSettings
     public static string SiteLocation { get; } = "https://butterflyghost.azurewebsites.net/ghost/api/v3/content";
     public static string RestApiLocation { get; } = "https://butterflyghost.azurewebsites.net/ghost/api/v3/content";
  
-    public static string RestApiKey { get; } = "09f5e9fff7fca4c605a68c5590";
+    public static string RestApiKey { get; } = "76d3d6a473672d40056954dcda";
 }
 }
