03c1d5720fced1d6580cd17d9addb194d3b3a4ad
Patrrck Kelly <hey@butterflydesin.me>
2020-12-25 22:04:41 -0700

    first commit

---
+++ Ghost/GhostSettings.cs
@@ -0,0 +1,10 @@
+namespace Ghost
+{
+public sealed class GhostSettings
+{
+    public static string SiteLocation { get; } = "https://butterflyghost.azurewebsites.net/ghost/api/v3/content";
+    public static string RestApiLocation { get; } = "https://butterflyghost.azurewebsites.net/ghost/api/v3/content";
+ 
+    public static string RestApiKey { get; } = "09f5e9fff7fca4c605a68c5590";
+}
+}
